16426454a491368a7d667825e7a21b482b837c12
Maxim Ermolaev <ermolaev.me@jhvn.tk>
2022-04-27 16:03:49 +0300

    Добавлен роутер от раздела с квартирами

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import api # API для ajax
 from account import auth # Аутентификация
 from application import app_index # Рендеры главной страницы
 from admin_section import admin, com_categories, staff# Раздел администратора
-from inspector import index, houses
+from inspector import index, houses, flats
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
