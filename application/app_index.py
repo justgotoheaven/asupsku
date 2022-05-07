0681d34d7e8438d3d44295f95973662ba6a1e4d0
Maxim Ermolaev <ermolaev.me@jhvn.tk>
2022-05-07 19:25:22 +0300

    Добавлена возможность передавать показания

--- application/app_index.py
+++ application/app_index.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from app import app, db # Точка входа в приложение Flask
-from flask import session, render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
-from models import Address, House, Counter
+from models import Address, House, Counter, Pokaz
 from forms import SetPokazForm
 
 # index page
@@ -52,16 +52,21 @@ def app_user_meters(id):
         return redirect(url_for('inspector_index'))
 
     meter = Counter.query.filter_by(id=id).limit(1).first()
+    meter_pokaz = db.session.query(Pokaz.amount).filter_by(counter=meter.id).order_by(Pokaz.id.desc()).limit(1).first()
+    meter_pokaz_data = 'нет данных'
+    if meter_pokaz:
+        meter_pokaz_data = meter_pokaz.amount
     flat_info = db.session.query(Address.house, Address.kv).filter_by(id=meter.flat).limit(1).first()
     house = db.session.query(House.adres).filter_by(id=flat_info.house).limit(1).first()
     return render_template('/jasny/user/meter_info.html',
                            page_name='Прибор учета',
                            username=current_user.min_name(),
                            meter=meter,
+                           pokaz=meter_pokaz_data,
                            full_address = '{} кв. {}'.format(house.adres, flat_info.kv))
 
 
-@app.route('/app/pokaz/<int:kvid>')
+@app.route('/app/pokaz/<int:kvid>', methods=['GET','POST'])
 @login_required
 def app_user_meters_addpokaz(kvid):
     if current_user.is_admin():
@@ -71,12 +76,35 @@ def app_user_meters_addpokaz(kvid):
 
     house = db.session.query(Address.house, Address.kv).filter_by(id=kvid).limit(1).first()
     house_adr = db.session.query(House.adres).filter_by(id=house.house).limit(1).first()
-    meters = db.session.query(Counter.id, Counter.name).filter_by(flat=kvid).all()
+    meters = db.session.query(Counter.id, Counter.name, Counter.approved).filter_by(flat=kvid).all()
     meters_form_data = list()
     for m in meters:
         m_info = dict(counter_name=m.name, counter=m.id)
+        if m.approved:
+            pokaz = db.session.query(Pokaz.amount).filter_by(counter=m.id).order_by(Pokaz.id.desc()).limit(1).first()
+            if pokaz is None:
+                m_info['pokaz_current'] = 0.0
+            else:
+                m_info['pokaz_current'] = pokaz.amount
+        else:
+            continue
         meters_form_data.append(m_info)
     form = SetPokazForm(counters=meters_form_data)
+    if request.method == 'POST' and form.is_submitted():
+        data = form.counters.data
+        for pkz in data:
+            if pkz['pokaz'] is None:
+                continue
+            pkz_to_save = Pokaz(counter=pkz['counter'],
+                                amount=pkz['pokaz'],
+                                added_by=current_user.id)
+            try:
+                db.session.add(pkz_to_save)
+                db.session.commit()
+                flash('Показания счетчика {} успешно переданы!'.format(pkz['counter_name']), 'alert alert-success')
+            except Exception as e:
+                db.session.rollback()
+                flash('Ошибка при передачи показаний счетчика {}. Техническая информация: {}'.format(pkz['counter_name'], e), 'alert alert-danger')
     return render_template('/jasny/user/add_pkz.html',
                            pagename='Передача показаний',
                            username=current_user.min_name(),
